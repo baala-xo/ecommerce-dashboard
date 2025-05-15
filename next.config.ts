8e44ad35de64f70b7c83b196e416f0af48bb9b9e
bala <bala2003fd@gmail.com>
2025-05-15 10:25:34 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    domains: ['i.pravatar.cc'],
+   },
+   experimental:{
+    ppr:'incremental'
+   },
 };
 
 export default nextConfig;
